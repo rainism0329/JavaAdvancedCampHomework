053de0d66792af2d6701e202ab9c2a7fc9188093
Phil Zhang <371418433@qq.com>
2021-09-27 02:46:24 +0800

    add hmilydemo

---
+++ Week_08/hmilydemo/src/test/java/com/example/hmilydemo/HmilydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hmilydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmilydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
